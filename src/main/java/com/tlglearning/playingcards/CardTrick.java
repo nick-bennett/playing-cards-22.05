11b2c111e30d6a1612f516dc245f38f23c176072
Nick Bennett <nick@nickbenn.com>
2022-06-21 15:37:33 -0600

    Outlined the high-level tasks.

--- src/main/java/com/tlglearning/playingcards/CardTrick.java
+++ src/main/java/com/tlglearning/playingcards/CardTrick.java
@@ -1,9 +1,20 @@
 package com.tlglearning.playingcards;
 
+import com.tlglearning.playingcards.model.Card;
+import com.tlglearning.playingcards.model.Deck;
+
+import java.util.Deque;
+
 public class CardTrick {
+
+    private Deque<Card> blackPile;
+    private Deque<Card> redPile;
+
 
     public static void main(String[] args) {
         // TODO Create an instance of Deck and shuffle it (or not).
+        Deck deck = new Deck();
+        deck.shuffle();
         // TODO Draw cards from the deck, according to the instructions, placing every other card into a red deque and a
         //  black deque.
         // TODO (Optional: Shuffle the red deque and black deque.)
