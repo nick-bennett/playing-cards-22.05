ae136cbbefaecdb3704af833a33ec6d27b7f19c2
Nick Bennett <nick@nickbenn.com>
2022-06-22 11:04:34 -0600

    Made comparator an anonymous class in tally.

--- src/main/java/com/tlglearning/playingcards/CardTrick.java
+++ src/main/java/com/tlglearning/playingcards/CardTrick.java
@@ -80,7 +80,18 @@ public class CardTrick {
 
         }
 
-        Comparator<Card> comparator = new DisplayComparator();
+        Comparator<Card> comparator = new Comparator<>() {
+
+            @Override
+            public int compare(Card card1, Card card2) {
+                int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());
+                comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
+                comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
+                return comparison;
+            }
+
+        };
+
         Collections.sort((LinkedList<Card>) blackPile, comparator);
         Collections.sort((LinkedList<Card>) redPile, comparator);
         System.out.printf("Black: count=%d, cards=%s%n", blackCount, blackPile);
